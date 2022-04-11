e65fcaa11232afce63737b4a9de4625c0a1da14e
NovaNo1r <mail@novanoir.dev>
2022-04-11 13:35:04 +0800

    Optimized config construction.

--- src/config.py
+++ src/config.py
@@ -5,8 +5,9 @@ WEBVPN_USER_INFO_URL = WEBVPN_BASE_URL + '/user/info'
 
 JWGL_BASE_URL = "https://jwgl.bupt.edu.cn"
 JWGL_JSXSD_URL = JWGL_BASE_URL + "/jsxsd"
+JWGL_FRAME_URL = JWGL_JSXSD_URL + "/framework"
 JWGL_LOGON_DO_URL = JWGL_BASE_URL + "/Logon.do?method=logon"
 JWGL_LOGIN_TO_XK_URL = JWGL_JSXSD_URL + "/xk/LoginToXk"
-JWGL_XS_MAIN_URL = JWGL_LOGIN_TO_XK_URL + "/framework/xsMain.jsp"
-JWGL_KB_URL = JWGL_JSXSD_URL + "/framework/main_index_loadkb.jsp"
-JWGL_XS_MAIN_NEW_URL = JWGL_JSXSD_URL + "/framework/xsMain_new.jsp?t1=1"
+JWGL_XS_MAIN_URL = JWGL_FRAME_URL + "/xsMain.jsp"
+JWGL_KB_URL = JWGL_FRAME_URL + "/main_index_loadkb.jsp"
+JWGL_XS_MAIN_NEW_URL = JWGL_FRAME_URL + "/xsMain_new.jsp?t1=1"
